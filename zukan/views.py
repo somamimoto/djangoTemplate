cd7886f12fba41803fc4f44da91ad2945782813a
somamimoto <happyvetlife@gmail.com>
2023-06-26 15:24:48 +0900

    Pagination has been implemented

--- zukan/views.py
+++ zukan/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models.cat_models import CatZukan
 from .models.dog_models import DogZukan
 from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def index(request, category_slug):
@@ -10,6 +11,16 @@ def index(request, category_slug):
             zukan = DogZukan.objects.filter(is_public=True)
         else:
             zukan = CatZukan.objects.filter(is_public=True)
+        page = request.GET.get('page', 1)
+        paginator = Paginator(zukan, per_page=5, orphans=2)
+        # We have implemented try-except clause as django original source code does
+        # https://github.com/django/django/blob/main/django/core/paginator.py
+        try:
+            zukan = paginator.page(page)
+        except PageNotAnInteger:
+            zukan = paginator.page(1)
+        except EmptyPage:
+            zukan = paginator.page(paginator.num_pages)
         context = {
             'category': category_slug,
             'zukan': zukan,
