b1c018187f07c4b7dcbf829fb8ae949e39e27c22
somamimoto <happyvetlife@gmail.com>
2023-06-29 15:43:36 +0900

    csv export functionality

--- csvapp/forms.py
+++ csvapp/forms.py
@@ -4,4 +4,8 @@ from django.core.validators import FileExtensionValidator
 
 class CsvFileUploadForm(forms.Form):
     file = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['csv'], message='Invalid file')], required=True)
-    has_header = forms.BooleanField(widget=forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]), required=False)
+    has_header = forms.ChoiceField(widget=forms.RadioSelect, choices=[('True', 'Yes'), ('False', 'No')], required=False)
+
+
+class CsvFileExportForm(forms.Form):
+    select_data = forms.ChoiceField(widget=forms.RadioSelect, choices=[('product_data', 'Product Data'), ('saleshistory_data', 'Sales History Data')])
--- csvapp/views.py
+++ csvapp/views.py
@@ -1,9 +1,10 @@
 import io
 import csv
 from datetime import datetime
-from django.shortcuts import render
-from .forms import CsvFileUploadForm
+from django.shortcuts import render, redirect
+from .forms import CsvFileUploadForm, CsvFileExportForm
 from .models import Product, SalesHistory
+from django.contrib import messages
 
 
 def csv_import(request):
@@ -15,7 +16,8 @@ def csv_import(request):
 
             # To bulk insert database, organize the file data
             saleshistory_instances = []
-            if form.cleaned_data['has_header']:  # Once form is_valid, it will have cleaned_data
+            # When You want to check if csv has header, you can consider to use csv.Sniffer
+            if form.cleaned_data['has_header'] == 'True':  # Once form is_valid, it will have cleaned_data
                 reader = csv.DictReader(file)
                 for row in reader:
                     product_instance, _ = Product.objects.get_or_create(name=row['product'])
@@ -49,5 +51,35 @@ def csv_import(request):
 
 
 def csv_export(request):
-    context = {}
+    if request.method == 'POST':
+        form = CsvFileExportForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['select_data'] == 'product_data':
+                object_list = Product.objects.all()
+            else:
+                object_list = SalesHistory.objects.all()
+            _object_list = list(object_list)
+            header_cols = [field for field in _object_list[0].__dict__.keys() if field not in ['_state', 'id']]
+            # If You want to get fields name from Model itself, use MyModel.model._meta.fileds
+            rows = []
+            for obj in _object_list:
+                row_vals = []
+                for h_col in header_cols:
+                    row_vals.append(getattr(obj, h_col))
+                rows.append(row_vals)
+            with open('export.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerow(header_cols)
+            with open('export.csv', 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerows(rows)
+                messages.success(request, 'Csv export has been done!')
+                return redirect('csvapp:csv_export')
+        else:
+            print(form.errors)
+    else:
+        form = CsvFileExportForm()
+    product_instances = Product.objects.all()
+    saleshistory_instances = SalesHistory.objects.all()
+    context = {'product_instances': product_instances, 'saleshistory_instances': saleshistory_instances, 'form': form}
     return render(request, 'csvapp/csv_export.html', context)
